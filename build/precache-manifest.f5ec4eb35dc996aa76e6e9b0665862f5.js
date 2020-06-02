937213688e61810c536fc84d3c97bd315b4820e5
Chikku <chikkuspanicker@gmail.com>
2020-06-02 20:07:36 +0530

    Build

---
+++ build/precache-manifest.f5ec4eb35dc996aa76e6e9b0665862f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879fcfde00106fd892463ce8daf73e87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "931255767ac454487638",
+    "url": "./static/css/main.edb5f846.chunk.css"
+  },
+  {
+    "revision": "4240564e78f50be4938e",
+    "url": "./static/js/2.4cca6f87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4cca6f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931255767ac454487638",
+    "url": "./static/js/main.866a742c.chunk.js"
+  },
+  {
+    "revision": "d71cfe67febd287ef028",
+    "url": "./static/js/runtime-main.d2ee63b5.js"
+  }
+]);
